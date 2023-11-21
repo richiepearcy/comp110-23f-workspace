3cdf6187fbb85bb4338e8518d95bb7c697d00ab5
Richard Pearcy <richiep@live.unc.edu>
2023-11-21 05:22:29 -0500

    F

--- exercises/ex08/river_simulation.py
+++ exercises/ex08/river_simulation.py
@@ -6,7 +6,7 @@ class my_river:
     fish: int
     bears: int
 
-    def __init__(self, fish_init: int, bears_init: int):
+    def __init__(self, day_init: int, fish_init: int, bears_init: int):
         self.fish: int = 10
         self.bears: int = 2
         self.day: int = 0
